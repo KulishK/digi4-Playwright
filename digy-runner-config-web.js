acd8d0f28fb4c04698a8f7b55cc2d37918292aeb
Kulshrestha <Kulish.Kulshrestha@franklintempleton.com>
2023-04-13 17:18:50 +0530

    initial commit

---
+++ digy-runner-config-web.js
@@ -0,0 +1,21 @@
+const digyRunnerConfig = {
+
+    env: {
+    PROJECT_NAME: "Playwright-js-poc",
+    TEAM_NAME: "FT TQA Playwright",
+    SUITE_NAME: "Demo Regression WEB",
+    APP_VERSION: "2.0",
+    ENVIRONMENT: "test",
+    FRAMEWORK: "playwright",
+    MODULE_NAME: "SomeModuleName",
+    TESTER: "Joe Bloggs",
+    BA: "Joe Bloggs",
+    DEVELOPER: "Joe Bloggs",
+    TEST_TYPE: "WEB",
+    RESULTS_SUMMARY_URL: `https://rcr0bssa51.execute-api.uswest-2.amazonaws.com/digykube-dev/v3/resultsSummary`,
+    RESULTS_URL: `https://rcr0bssa51.execute-api.us-west2.amazonaws.com/digykube-dev/v3/results`,
+    DIGY4_API_KEY: "",
+    DIGY4_API_SECRET: "",
+    },
+   };
+   module.exports = digyRunnerConfig;
